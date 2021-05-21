c69dea0a8a90fab2e7063e41842ba60a25e33da2
Noah Zarro <noahzarro@gmail.com>
2021-05-21 14:48:47 +0200

    adapted node server for use with nginx

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const helmet = require("helmet")
 
-let key;
-let cert;
-
-try {
-    key = fs.readFileSync("key.pem");
-    cert = fs.readFileSync("cert.pem");
-} catch {
-    key = fs.readFileSync(
-        "/etc/letsencrypt/live/eth-lerngruppe.ch/privkey.pem"
-    );
-    cert = fs.readFileSync(
-        "/etc/letsencrypt/live/eth-lerngruppe.ch/fullchain.pem"
-    );
-}
-
 const app = express();
-
 
 app.use(helmet.hsts({
     maxAge: 15552000,
@@ -28,5 +10,3 @@ app.use(helmet.hsts({
 app.use(express.static('public/elv-web-data'));
 
 app.listen(8080);
-
-https.createServer({key, cert}, app).listen(8443);
